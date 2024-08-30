27c10f875c62d07a023dba68beee6d68c046902b
AxelCarayon <55441727+AxelCarayon@users.noreply.github.com>
2024-08-30 16:17:04 +0200

    ajout script

---
+++ publication_URL_to_UPS_URL.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         publication URL to UPS URL
+// @namespace    None
+// @version      2024-04-29
+// @description  Replace a publication URL with Paul Sabatier corresponding URL
+// @author       You
+// @match        https://www.sciencedirect.com/*
+// @match        https://dl.acm.org/*
+// @match        https://ieeexplore.ieee.org/*
+// @match        https://link.springer.com/*
+// @grant        none
+// ==/UserScript==
+
+
+const arr = [
+    { key: 'https://www.sciencedirect.com', value: 'https://www-sciencedirect-com.gorgone.univ-toulouse.fr' },
+    { key: 'https://dl.acm.org', value: 'https://dl-acm-org.gorgone.univ-toulouse.fr' },
+    { key: 'https://ieeexplore.ieee.org', value: 'https://ieeexplore-ieee-org.gorgone.univ-toulouse.fr' },
+    { key: 'https://link.springer.com', value:'https://link-springer-com.gorgone.univ-toulouse.fr'}
+  ];
+  const urls = new Map(arr.map((obj) => [obj.key, obj.value]));
+
+
+(function() {
+    let origin = window.location.origin;
+    let pathName = window.location.pathname;
+    window.location.assign(urls.get(origin)+pathName)
+})();
